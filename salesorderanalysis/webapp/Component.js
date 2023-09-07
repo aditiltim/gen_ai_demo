674dee19be431a94256c52d114f54651856bec6d
Hemant Upadhyay <hemant1.upadhyay@ltimindtree.com>
2023-09-07 08:12:49 +0000

    sales order changes

--- salesorderanalysis/webapp/Component.js
+++ salesorderanalysis/webapp/Component.js
@@ -3,33 +3,33 @@
  */
 
 sap.ui.define([
-        "sap/ui/core/UIComponent",
-        "sap/ui/Device",
-        "hac2build/salesorderanalysis/model/models"
-    ],
-    function (UIComponent, Device, models) {
-        "use strict";
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "hac2build/salesorderanalysis/model/models"
+],
+function (UIComponent, Device, models) {
+    "use strict";
 
-        return UIComponent.extend("hac2build.salesorderanalysis.Component", {
-            metadata: {
-                manifest: "json"
-            },
+    return UIComponent.extend("hac2build.salesorderanalysis.Component", {
+        metadata: {
+            manifest: "json"
+        },
 
-            /**
-             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-             * @public
-             * @override
-             */
-            init: function () {
-                // call the base component's init function
-                UIComponent.prototype.init.apply(this, arguments);
+        /**
+         * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+         * @public
+         * @override
+         */
+        init: function () {
+            // call the base component's init function
+            UIComponent.prototype.init.apply(this, arguments);
 
-                // enable routing
-                this.getRouter().initialize();
+            // enable routing
+            this.getRouter().initialize();
 
-                // set the device model
-                this.setModel(models.createDeviceModel(), "device");
-            }
-        });
-    }
+            // set the device model
+            this.setModel(models.createDeviceModel(), "device");
+        }
+    });
+}
 );
