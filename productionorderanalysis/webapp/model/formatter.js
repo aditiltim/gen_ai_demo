e90120b4169df7f2364e40328c7826b0e03581e0
DarshanR Shah <darshanr.shah@ltimindtree.com>
2023-09-09 13:17:28 +0000

    Impacting factor Ui update

--- productionorderanalysis/webapp/model/formatter.js
+++ productionorderanalysis/webapp/model/formatter.js
@@ -2,33 +2,18 @@ sap.ui.define(function() {
     'use strict';
     
     return {
+        // formatter for setting date color on realistic generated date
         statusText: function(genDate, delDate){
             var oDelDate = new Date(delDate);
             var oGenDate = new Date(genDate);
-            var diffDays = oGenDate.getDate() - oDelDate.getDate();
+            var diffDays = parseInt((oGenDate - oDelDate) / (1000 * 60 * 60 * 24), 10); 
+            // var diffDays = oGenDate.getDate() - oDelDate.getDate();
             if(diffDays > 0){
                 return "Error";
             } else if(diffDays <= 0){
                 return "Success";
             } else{
                 return "None";
-            }
-        },
-        statusTextSentiment: function(sentiment){
-            if(sentiment>=0 && sentiment<=30){
-                return "Success";
-            } else if(sentiment>30 && sentiment<=55){
-                return "Information";
-            } else if(sentiment>55){
-                return "Error";
-            }
-        },
-
-        setIcon: function(sentiment){
-            if (sentiment >=0 && sentiment<=25) {
-                return "sap-icon://bus-public-transport";
-            } else if(sentiment > 25 && sentiment <40) {
-                return 
             }
         }
     };
